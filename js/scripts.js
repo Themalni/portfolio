7f469426b85f5aade9fd340726232862d33bb08b
Alex Efremov <alex.efremov.dev@gmail.com>
2017-02-10 23:58:44 +0200

    Check if DOM is ready, wrap all code in a callback function

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,77 @@
-var header = document.querySelector(".header");
-var navHamburger = document.querySelector(".nav__hamburger");
-var navMenu = document.querySelector(".nav-menu");
-var navMenuCross = document.querySelector(".nav-menu__cross");
-var navMenuListItem = document.querySelectorAll(".nav-menu__list-item");
-var navMenuSeparator = document.querySelector(".nav-menu__separator");
-var navMenuText = document.querySelector(".nav-menu__text");
-var navMenuEmail = document.querySelector(".nav-menu__email");
-var navMenuSocialBtn = document.querySelector(".nav-menu__social-btn");
-var i;
-var itemsLength = navMenuListItem.length;
-var listItem = "";
+function onReady() { // Handler when the DOM is fully loaded
+  var header = document.querySelector(".header");
+  var navHamburger = document.querySelector(".nav__hamburger");
+  var navMenu = document.querySelector(".nav-menu");
+  var navMenuCross = document.querySelector(".nav-menu__cross");
+  var navMenuListItem = document.querySelectorAll(".nav-menu__list-item");
+  var navMenuSeparator = document.querySelector(".nav-menu__separator");
+  var navMenuText = document.querySelector(".nav-menu__text");
+  var navMenuEmail = document.querySelector(".nav-menu__email");
+  var navMenuSocialBtn = document.querySelector(".nav-menu__social-btn");
+  var i;
+  var itemsLength = navMenuListItem.length;
+  var listItem = "";
 
-/* show menu items */
-function animateItems(){
-  for(i = 0; i < itemsLength; i++){
-    listItem = navMenuListItem[i];
-    if(listItem.classList.contains("nav-menu__items__animation")){
-      listItem.classList.remove("nav-menu__items__animation");
-    }else{
-      listItem.classList.add("nav-menu__items__animation");
+  /* show menu items */
+  function animateItems(){
+    for(i = 0; i < itemsLength; i++){
+      listItem = navMenuListItem[i];
+      if(listItem.classList.contains("nav-menu__items__animation")){
+        listItem.classList.remove("nav-menu__items__animation");
+      }else{
+        listItem.classList.add("nav-menu__items__animation");
+      }
     }
   }
-}
-function animateCross(item){
-  if(item.classList.contains("nav-menu__cross__animation")){
-    item.classList.remove("nav-menu__cross__animation");
-  }else{
-    item.classList.add("nav-menu__cross__animation");
+  function animateCross(item){
+    if(item.classList.contains("nav-menu__cross__animation")){
+      item.classList.remove("nav-menu__cross__animation");
+    }else{
+      item.classList.add("nav-menu__cross__animation");
+    }
   }
-}
-function animateElement(item){
-  if(item.classList.contains("nav-menu__items__animation")){
-    item.classList.remove("nav-menu__items__animation");
-  }else{
-    item.classList.add("nav-menu__items__animation");
+  function animateElement(item){
+    if(item.classList.contains("nav-menu__items__animation")){
+      item.classList.remove("nav-menu__items__animation");
+    }else{
+      item.classList.add("nav-menu__items__animation");
+    }
   }
-}
-function toggleAnimation(){
-  animateItems();
-  animateElement(navMenuSeparator);
-  animateElement(navMenuText);
-  animateElement(navMenuEmail);
-  animateElement(navMenuSocialBtn);
-  animateCross(navMenuCross);
-}
+  function toggleAnimation(){
+    animateItems();
+    animateElement(navMenuSeparator);
+    animateElement(navMenuText);
+    animateElement(navMenuEmail);
+    animateElement(navMenuSocialBtn);
+    animateCross(navMenuCross);
+  }
 
-window.onload = fadeInHeader;
-navHamburger.addEventListener("click", showMenu);
-navMenuCross.addEventListener("click", hideMenu);
+  window.onload = fadeInHeader;
+  navHamburger.addEventListener("click", showMenu);
+  navMenuCross.addEventListener("click", hideMenu);
 
-/* show header */
-function fadeInHeader(){
-  header.style.opacity = 1;
-}
+  /* show header */
+  function fadeInHeader(){
+    header.style.opacity = 1;
+  }
 
-/* show menu */
-function showMenu(){
-  navHamburger.style.visibility = "hidden";
-  navMenu.style.width = "250px";
-  navMenuCross.style.display = "block";
-  toggleAnimation();
-}
+  /* show menu */
+  function showMenu(){
+    navHamburger.style.visibility = "hidden";
+    navMenu.style.width = "250px";
+    navMenuCross.style.display = "block";
+    toggleAnimation();
+  }
 
-/* hide menu */
-function hideMenu(){
-  navMenuCross.style.display = "none";
-  navMenu.style.width = "0px";
-  navHamburger.style.visibility = "visible";
-  toggleAnimation();
-}
+  /* hide menu */
+  function hideMenu(){
+    navMenuCross.style.display = "none";
+    navMenu.style.width = "0px";
+    navHamburger.style.visibility = "visible";
+    toggleAnimation();
+  }
 
-
-/*========= jQuery ==========*/
-
-/* Scroll to a section */
-$(document).ready(function($){
-
+  /* Scroll to a section */
   $(".nav-menu__link").click(function(){
     hideMenu();
     $("html, body").animate({
@@ -90,4 +86,17 @@ $(document).ready(function($){
     }, 800);
     return false;
   });
-});
+
+}
+
+// Vanilla JS analogue $(document).ready
+(function() {
+  if (
+      document.readyState === "complete" ||
+      (document.readyState !== "loading" && !document.documentElement.doScroll)
+  ) {
+    onReady();
+  } else {
+    document.addEventListener("DOMContentLoaded", onReady);
+  }
+})();
