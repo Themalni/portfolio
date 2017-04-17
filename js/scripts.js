cacff4d44fdbe9232864b0f8292fed22cf7e06e5
Themalni <themalni@live.co.uk>
2017-04-18 00:00:27 +0200

    added minified about-me stiles

--- js/scripts.js
+++ js/scripts.js
@@ -257,14 +257,14 @@ function onReady() { // Handler when the DOM is fully loaded
 
   /* Scroll to a section */
   function addScrollEffects() {
-    $(".nav-menu__link, .nav__logo__link").click(function(){
+    $(".nav-menu__link").click(function(){
       hideMenu();
       $("html, body").animate({
         scrollTop: $($.attr(this, "href")).offset().top
       }, 800);
       return false;
     });
-    $(".header__btn, .footer-menu__link").click(function(){
+    $(".nav__logo__link, .header__btn, .footer-menu__link").click(function(){
       $("html, body").animate({
         scrollTop: $($.attr(this, "href")).offset().top
       }, 800);
